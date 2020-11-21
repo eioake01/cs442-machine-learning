f5c40f9c8a6331056cd145472d758e702b1cd486
Eleni Ioakeim <eioake01@cs.ucy.ac.cy>
2020-11-21 17:44:19 +0200

    implement algorithm, need to fix output data

--- Assignment_4/Assignment_4.py
+++ Assignment_4/Assignment_4.py
@@ -1,4 +1,4 @@
-import numpy as np
+import numpy as np,matplotlib.pyplot as plt
 
 def readParameters():
     parameters = open('parameters.txt',"r")
@@ -30,17 +30,65 @@ def readSigmas(file):
     return np.loadtxt(file,usecols=(0),dtype=float)
 
 def readCentres(file):
-    return np.loadtxt(file,usecols=(0),dtype=float)
+    return np.loadtxt(file,delimiter=",",dtype=float)
+
+def euclideanDistance(inputInstance,centres):
+    return np.sum(np.square(inputInstance-centres),axis=1,keepdims=True)
+
+def gaussianFunction(inputInstance,centres,sigmas):
+    denominator = 2 * np.square(sigmas)
+    return np.exp(-euclideanDistance(inputInstance,centres)/denominator)
+
+def plotError(error):
+    error = np.array(error)
+    plt.figure(1)
+    plt.plot(error[:, 0], error[:, 1], label="Train Error")
+    plt.plot(error[:, 0], error[:, 2], label="Test Error")
+    plt.xlabel("Epochs")
+    plt.ylabel("Error Values")
+    plt.title("Error Graph")
+    plt.legend()
 
 class RBF:
     def __init__(self,numOfInputNeurons,numHiddenLayerNeurons,numOutputNeurons,learningRates,sigmas,initCentres):
-        self.coefficientLearningRate = initLearningRates[0]
-        self.centresLearningRate = initLearningRates[1]
-        self.sigmasLearningRate = initLearningRates[2]
+        self.coefficientLearningRate = learningRates[0]
+        self.centresLearningRate = learningRates[1]
+        self.sigmasLearningRate = learningRates[2]
 
-        self.sigmas = sigmas
+        self.coefficients = np.random.uniform(low=-1.0,high=1.0,size=(numOutputNeurons,numHiddenLayerNeurons)).T
+        self.biasCoefficient = np.random.uniform(low=-1.0,high=1.0)
         self.centres = initCentres
+        self.sigmas = sigmas
+        
 
+        self.outputLayer = None
+
+    def forwardPass(self,inputData):
+        self.outputLayer =  np.zeros((inputData.shape[0],1))
+
+        for i in range(inputData.shape[0]):
+            gaussian = gaussianFunction(inputData[i],self.centres,self.sigmas)
+            self.outputLayer[i] = self.biasCoefficient + np.sum(gaussian * self.coefficients)
+
+    def updateVariables(self,inputData,outputData):
+        for i in range(inputData.shape[0]):
+            inputInstance = inputData[i]
+
+            error = outputData[i] - self.outputLayer[i]
+            gaussian = gaussianFunction(inputInstance,self.centres,self.sigmas)
+            commonPart = self.coefficients*gaussian
+
+            partOfCentresUpdate = commonPart *((inputInstance-self.centres)/np.square(self.sigmas))
+            partOfSigmasUpdate = commonPart * (euclideanDistance(inputInstance,self.centres)/np.power(self.sigmas,3))
+
+            self.coefficients += (self.coefficientLearningRate*error*gaussian)
+            self.biasCoefficient += (self.coefficientLearningRate * error)
+            self.centres += (self.centresLearningRate * partOfCentresUpdate)
+            self.sigmas += (self.sigmasLearningRate*partOfSigmasUpdate)
+
+    def error(self,outputData):
+        return np.square(self.outputLayer-outputData)/2
+ 
 
 
 if __name__ == "__main__":  
@@ -56,6 +104,39 @@ if __name__ == "__main__":
 
     learningRates = readLearningRates(parameters[3])
     sigmas = readSigmas(parameters[4])[np.newaxis].T
-    initCentres = readCentres(parameters[6])[np.newaxis].T
+    initCentres = readCentres(parameters[6])
+
+    if(initCentres.shape[0]!=parameters[1]):
+        print("The number of centres should be the same as the number of neuros of the hidden layer.")
+        quit()
+
+    if(sigmas.shape[0]!=parameters[1]):
+        print("The number of sigma values should be the same as the number of neuros of the hidden layer.")
+        quit()
+    
 
     RBFnetwork = RBF(parameters[0],parameters[1],parameters[2],learningRates,sigmas,initCentres)
+
+    error =  []   
+    for epoch in range(parameters[-4]):
+        # Forward pass with train data
+        RBFnetwork.forwardPass(trainInputData)
+        # Calculate train error
+        trainError = RBFnetwork.error(trainOutputData)
+
+        # Update variables
+        RBFnetwork.updateVariables(trainInputData,trainOutputData)
+
+        # Forward pass with test data
+        RBFnetwork.forwardPass(testInputData)
+        # Calculate test error
+        testError = RBFnetwork.error(testOutputData)
+        
+        error.append([epoch+1,round(trainError,4),round(testError,4)])
+
+    allCoefficients = np.append(RBFnetwork.coefficients,RBFnetwork.biasCoefficient)
+
+    np.savetxt("error.txt",error,fmt='%i %.4f %.4f',delimiter='\t')
+    np.savetxt("coefficients.txt",allCoefficients,fmt="%.4f",delimiter=",")
+    plotError(error)
+    plt.show()
